a4dfe1fe79ad5235b43a5b517f676a9d96d75a0c
gamagegit <cgamage92@yahoo.com>
2022-01-17 22:03:50 -0800

    initial commit

---
+++ src/context.js
@@ -0,0 +1,43 @@
+import React, { useState, useContext, useEffect } from "react";
+// make sure to use https
+export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`;
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState({ show: false, msg: "" });
+  const [query, setQuery] = useState("sup");
+
+  const fetchMovies = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(`${API_ENDPOINT}&s=${query}`);
+      const data = await response.json();
+      if (data.Response === "True") {
+        setMovies(data.Search);
+        setError({ show: false, msg: "" });
+      } else {
+        setError({ show: true, msg: data.Error });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, [query]);
+  return (
+    <AppContext.Provider value={{ isLoading, movies, error, query, setQuery }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+// make sure use
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
